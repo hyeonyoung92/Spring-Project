4bf616ab483a412cbde5d15200462807aa697967
hyeonyoung92 <hyeonyounglee@gmail.com>
2017-06-22 14:08:11 +0900

    extract duplicated method

--- src/main/java/kr/co/hyeonyoung/dao/DaoFactory.java
+++ src/main/java/kr/co/hyeonyoung/dao/DaoFactory.java
@@ -4,18 +4,18 @@ public class DaoFactory {
 	/* UserDao가 아닌 다른 DAO의 생성기능을 넣으면?? */
 	/* ConnetionMaker 구현클래스 선정 및 생성에서 중복이 발생함 */
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
-		
-		return userDao;
+		return new UserDao(connectionMaker());
 	}
 	
 	public AccountDao accountDao() {
-		return new AccountDao(new DConnectionMaker());
+		return new AccountDao(connectionMaker());
 	}
 	
 	public MessageDao messageDao() {
-		return new MessageDao(new DConnectionMaker());
+		return new MessageDao(connectionMaker());
 	}
 	
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
+	}
 }
