cb8047534f709a669e30dce1a749f7a707ad9395
hyeonyoung92 <hyeonyounglee@gmail.com>
2017-06-23 11:38:06 +0900

    add the comment

--- src/main/java/kr/co/hyeonyoung/dao/UserDaoTest.java
+++ src/main/java/kr/co/hyeonyoung/dao/UserDaoTest.java
@@ -10,6 +10,13 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		/*
+		 1. 자바에서 손쉽게 실행 가능한  main()을 이용
+		 2. 테스트할 대상인  UserDao의 오브젝트를 가져와 메소드를 호출
+		 3. 테스트에 사용할 입력 값을 코드에서 만들어 넣어줌
+		 4. 테스트의 결과를 콘솔에 출력
+		 5. 각 단계의 작업이 에러 없이 끝나면 콘솔에 성공 메시지 출력
+		*/
 		/*ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);*/
 		ApplicationContext context = new GenericXmlApplicationContext("kr/co/hyeonyoung/dao/applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
